c120a5c4430af325f64a1bf39c01b5fc37f6b4d5
MakDerive <kirillandmax21@gmail.com>
2024-11-02 20:31:56 +0300

    FraudDetectorTest

--- src/main/java/FraudDetector.java
+++ src/main/java/FraudDetector.java
@@ -2,7 +2,7 @@ package main.java;
 
 public class FraudDetector {
 
-    boolean isFraud(Transaction transaction) {
+    public boolean isFraud(Transaction transaction) {
     	if (transaction.getTrader().getFullName().equals("Pokemon")) {
 			return true;
 		}
--- src/test/java/FraudDetectorTest.java
+++ src/test/java/FraudDetectorTest.java
@@ -1,5 +1,21 @@
 package test.java;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.java.FraudDetector;
+import main.java.Trader;
+import main.java.Transaction;
+
 public class FraudDetectorTest {
-	
+	Trader trader = new Trader("Pokemon2", "Moscow");
+	Transaction transaction = new Transaction(trader, 10);
+	@Test 
+	void isFraud() {
+		FraudDetector fraudDetector = new FraudDetector();
+		boolean fraud = fraudDetector.isFraud(transaction);
+		Assertions.assertFalse(fraud);
+
+	}
 }
